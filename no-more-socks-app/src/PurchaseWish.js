9753655463eeca56abb51883ddd8c1d21d80bcd7
Rajea <skandislamic@gmail.com>
2024-01-20 15:33:26 +0000

    hover added to btns

--- no-more-socks-app/src/PurchaseWish.js
+++ no-more-socks-app/src/PurchaseWish.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 export default function PurchaseWish() {
   return (
     <>
-      <Link to="/mum"><button className="font-bold py-3 bg-red-500 rounded-md px-4 text-stone-50">Purchase a Wish</button></Link>
+      <Link to="/mum"><button className="font-bold py-3 bg-red-500 rounded-md px-4 text-stone-50 btn">Purchase a Wish</button></Link>
     </>
   );
 }
