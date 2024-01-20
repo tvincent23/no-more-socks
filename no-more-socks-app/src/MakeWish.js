e2d8c5cf31fdcaeec455fbfd7738b2e5fd217a5c
Rajea <skandislamic@gmail.com>
2024-01-20 14:33:56 +0000

    slight changes

--- no-more-socks-app/src/MakeWish.js
+++ no-more-socks-app/src/MakeWish.js
@@ -1,7 +1,11 @@
-export default function MakeWish(prop)
+export default function MakeWish(prop) {
+
   return (
     <>
       <button className="font-bold py-3 bg-red-500 rounded-md px-4 text-stone-50">Make a Wish</button>
     </>
   );
+
 }
+
+
--- no-more-socks-app/src/PurchaseWish.js
+++ no-more-socks-app/src/PurchaseWish.js
@@ -1,7 +1,7 @@
 export default function PurchaseWish() {
   return (
     <>
-      <button className="font-bold py-3 bg-red-500 rounded-md px-4 text-stone-50>Purchase a Wish</button>
+      <button className="font-bold py-3 bg-red-500 rounded-md px-4 text-stone-50">Purchase a Wish</button>
     </>
   );
 }
