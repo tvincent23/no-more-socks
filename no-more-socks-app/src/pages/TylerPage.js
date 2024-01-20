c14139fca8df3e2043c5e763f9cd4088cf8ea3e4
Thomas Vincent <thomasvincent@live.co.uk>
2024-01-20 14:44:48 +0000

    tylerpage update

--- no-more-socks-app/src/pages/TylerPage.js
+++ no-more-socks-app/src/pages/TylerPage.js
@@ -1,7 +1,19 @@
+import listData from "../data.json";
+
 export default function TylerPage() {
+  //   const tylerList = listData.map((item, index) => <li key={index}>{item}</li>);
+  console.log(listData);
   return (
     <>
-      <div>PLEASE</div>
+      <div className="flex">
+        <h2>Hi Tyler!</h2>
+        <ul>
+          {listData.map((item, index) => (
+            <li key={index}>{item.name}</li>
+          ))}
+          {/* <ul>{tylerList}</ul> */}
+        </ul>
+      </div>
     </>
   );
 }
