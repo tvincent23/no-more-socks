a138bc0bfde41743af99cd560df5ccd9be836b44
Rajea <skandislamic@gmail.com>
2024-01-20 16:15:13 +0000

    changes to the mums page

--- no-more-socks-app/src/pages/MumPage.js
+++ no-more-socks-app/src/pages/MumPage.js
@@ -1,7 +1,31 @@
+import { useState, useEffect } from "react";
+
 export default function MumPage() {
+
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch("http://localhost:8080/giver");
+      const data = await response.json();
+      console.log(data);
+      setData(data);
+    }
+
+    fetchData();
+  }, []);
+
   return (
-    <div className="mumpage">
-      <h1>MumPage</h1>
-    </div>
-  );
+    <>
+      <div className="flex">
+        <h2>Hi Mum!</h2>
+        <ul>
+          {data?.map((item, index) => (
+            <li key={index}>{item.name}</li>
+          ))}
+          {/* <ul>{tylerList}</ul> */}
+        </ul>
+      </div>
+    </>
+  )
 }
--- no-more-socks-backend/index.js
+++ no-more-socks-backend/index.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import cors from 'cors'
 
 
 // initialize the app
@@ -6,6 +7,7 @@ const app = express()
 const PORT = 8080
 
 app.use(express.json())
+app.use(cors())
 
 
 let data = [
@@ -13,24 +15,28 @@ let data = [
       "name": "Tyler",
       "gift": "bike",
       "id": "1234567890",
-      "comments": "This is a great gift for anyone who loves to bike. It's a new bike, so it's sure to be a hit!"
+      "comments": "This is a great gift for anyone who loves to bike. It's a new bike, so it's sure to be a hit!",
+      "purchased": false
+
     },
     {
       "name": "Tyler",
       "gift": "video game",
       "id": "9876543210",
-      "comments": "This is a great gift for anyone who loves video games. It's a new game, so it's sure to be a hit!"
+      "comments": "This is a great gift for anyone who loves video games. It's a new game, so it's sure to be a hit!",
+      "purchased": false
     },
     {
       "name": "Tyler",
       "gift": "shoes",
       "id": "0123456789",
-      "comments": "This is a great gift for anyone who loves shoes. It's a new pair of shoes, so it's sure to be a hit!"
+      "comments": "This is a great gift for anyone who loves shoes. It's a new pair of shoes, so it's sure to be a hit!",
+      "purchased": false
     }
 
   ]
 
-// set up the routes
+// Tyler's routes
 app.get('/receiver', async function(req, res) {
     console.log('get request received')
     res.status(200).json(data)
@@ -43,7 +49,31 @@ app.post('/receiver', async function(req, res) {
     const incomingGift = req.body
     data.push(incomingGift)
     res.status(200).json(data)
+})
 
+// app.delete('/receiver/:id', async function(req, res) {
+//     console.log(req.params.id)
+//     const id = req.params.id
+
+//     // return to me all the gifts whose id doesnt match with the id
+//     const updatedData = data.filter(giftObject => giftObject != id)
+//     res.status(200).json(updatedData)
+// })
+
+// Mom's routes
+app.get('/giver', async function(req, res) {
+    console.log('get request received for moms route')
+    res.status(200).json(data)
+})
+
+// mum's purchased gift route
+app.put('/giver/:id', async function(req, res) {
+    console.log('put request from mom received')
+    const id = req.params.id
+    const giftToBePurchased = data.find(giftObject => giftObject.id === id)
+    giftToBePurchased.purchased = true
+    console.log(giftToBePurchased)
+    res.status(200).json(data)
 })
 
 // Start the server and listen on the specified port
