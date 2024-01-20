c43637769c388c529f89ba6c6c82410601e953c4
Thomas <thomasvincent@live.co.uk>
2024-01-20 16:50:27 +0000

    tyler page progress

--- no-more-socks-app/src/pages/TylerPage.js
+++ no-more-socks-app/src/pages/TylerPage.js
@@ -3,28 +3,49 @@ import Styles from "./Tyler.css";
 
 export default function TylerPage() {
   const dataArray = listData.map((item, index) => (
-    <li key={index}>{item.gift}</li>
+    <article class="list" key={index}>
+      {item.gift}
+    </article>
   ));
 
   console.log(listData);
   return (
     <>
       <div>
-        <div>
-          <h2>Hi Tyler!</h2>
-        </div>
-        <div class="list">
+        <h2>Hi Tyler!</h2>
+      </div>
+
+      <section className="mainContainer">
+        <div className="giftsContainer">
           <ul>{dataArray}</ul>
         </div>
 
-        {/* <ul>
-          {listData.map((item, index) => (
-            <li key={index}>
-              {item.gift} {item.comments}
-            </li>
-          ))}
-        </ul> */}
-      </div>
+        {/* <form>
+          {/* <label htmlFor="giftName">Gift</label> */}
+        {/* <input type="text" id="giftName" placeholder="GiftName" /> */}
+        {/* </form>  */}
+
+        <form action="/submit" method="post">
+          <label for="giftName">Gift</label>
+          <input
+            type="text"
+            id="giftName"
+            name="giftName"
+            placeholder="Gift Name..."
+          />
+
+          <label for="giftDetails">Add Details</label>
+          <input
+            type="text"
+            id="giftDetails"
+            name="giftName"
+            placeholder="Add description/links..."
+          />
+          <button class="addGiftBtn" type="submit">
+            Add Gift
+          </button>
+        </form>
+      </section>
     </>
   );
 }
